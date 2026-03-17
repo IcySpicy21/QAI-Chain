e2b7536a21300136c2fa3e2043ed17cc3a49d369
maverick0721 <shivang0721gupta@gmail.com>
2026-03-17 19:57:24 +0000

    Created server to expose the node via API using FastAPI

---
+++ network/rpc_server.py
@@ -0,0 +1,94 @@
+from fastapi import FastAPI
+from core.blockchain.transaction import Transaction
+from core.blockchain.block import Block
+from core.blockchain.miner import proof_of_work
+
+app = FastAPI()
+
+node = None  # will be injected later
+
+
+def init_node(blockchain, mempool, p2p):
+    global node
+    node = {
+        "blockchain": blockchain,
+        "mempool": mempool,
+        "p2p": p2p
+    }
+
+@app.post("/transaction")
+def add_transaction(tx_data: dict):
+
+    tx = Transaction(
+        tx_data["sender"],
+        tx_data["receiver"],
+        tx_data["amount"],
+        tx_data.get("signature")
+    )
+
+    node["mempool"].add_transaction(tx)
+
+    node["p2p"].broadcast_transaction(tx)
+
+    return {"status": "transaction received"}
+
+@app.post("/block")
+def receive_block(block_data: dict):
+
+    blockchain = node["blockchain"]
+
+    block = Block(
+        index=block_data["index"],
+        transactions=[
+            Transaction(**t) for t in block_data["transactions"]
+        ],
+        previous_hash=block_data["previous_hash"]
+    )
+
+    block.nonce = block_data["nonce"]
+    block.hash = block_data["hash"]
+
+    if blockchain.is_valid_block(block, blockchain.last_block()):
+        blockchain.add_block(block)
+        return {"status": "block added"}
+
+    return {"status": "invalid block"}
+
+@app.get("/chain")
+def get_chain():
+
+    chain_data = []
+
+    for block in node["blockchain"].chain:
+        chain_data.append({
+            "index": block.index,
+            "hash": block.hash
+        })
+
+    return chain_data
+
+
+@app.post("/mine")
+def mine():
+
+    blockchain = node["blockchain"]
+    mempool = node["mempool"]
+
+    transactions = mempool.get_transactions()
+
+    if not transactions:
+        return {"status": "no transactions"}
+
+    new_block = Block(
+        index=len(blockchain.chain),
+        transactions=transactions,
+        previous_hash=blockchain.last_block().hash
+    )
+
+    mined_block = proof_of_work(new_block, blockchain.difficulty)
+
+    blockchain.add_block(mined_block)
+
+    node["p2p"].broadcast_block(mined_block)
+
+    return {"status": "block mined", "hash": mined_block.hash}

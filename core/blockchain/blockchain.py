6a8dc7a81312ff2acb95a4a82e94c5700c2d09ae
maverick0721 <shivang0721gupta@gmail.com>
2026-03-17 18:48:05 +0000

    Created chain of blocks

---
+++ core/blockchain/blockchain.py
@@ -0,0 +1,28 @@
+from core.blockchain.block import Block
+from core.utils.logger import get_logger
+
+logger = get_logger("blockchain")
+
+
+class Blockchain:
+
+    def __init__(self, difficulty=3):
+
+        self.chain = []
+        self.difficulty = difficulty
+
+        self.create_genesis_block()
+
+    def create_genesis_block(self):
+
+        genesis = Block(0, [], "0")
+
+        self.chain.append(genesis)
+
+    def last_block(self):
+
+        return self.chain[-1]
+
+    def add_block(self, block):
+
+        self.chain.append(block)

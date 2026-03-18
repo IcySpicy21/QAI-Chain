216772ba4a335415de14cc440bc587a2bc3e3433
maverick0721 <shivang0721gupta@gmail.com>
2026-03-18 13:04:17 +0000

    Updated transaction validation using PQC & block validation with signature checks

--- core/blockchain/blockchain.py
+++ core/blockchain/blockchain.py
@@ -1,5 +1,6 @@
 from core.blockchain.block import Block
-from core.utils.logger import get_logger
+from core.logger import get_logger
+from crypto.pqc.verification import verify_transaction
 
 logger = get_logger("blockchain")
 
@@ -13,28 +14,57 @@ class Blockchain:
 
         self.create_genesis_block()
 
+    
+    # Genesis Block
+    
     def create_genesis_block(self):
 
         genesis = Block(0, [], "0")
 
         self.chain.append(genesis)
 
+    
+    # Get last block
+    
     def last_block(self):
 
         return self.chain[-1]
 
+    
+    # Add block
+    
     def add_block(self, block):
 
         self.chain.append(block)
 
+    
+    # Transaction Validation (PQC)
+    
+    def validate_transaction(self, tx):
+
+        # sender is treated as public key
+        return verify_transaction(tx, tx.sender)
+
+    
+    # Block Validation
+    
     def is_valid_block(self, block, previous_block):
+
+        # check previous hash link
         if previous_block.hash != block.previous_hash:
             return False
 
+        # check PoW
         if not block.hash.startswith("0" * self.difficulty):
             return False
 
+        # check hash integrity
         if block.compute_hash() != block.hash:
             return False
+
+        # check transactions
+        for tx in block.transactions:
+            if not self.validate_transaction(tx):
+                return False
 
         return True
